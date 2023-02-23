d1dbb225400db2bbd875095e6cb0617c1975110c
Tai Nguyen <nphattai98@gmail.com>
2023-02-23 22:15:37 +0700

    feat: api health check

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { authRoutes } from '@auth/routes/auth.routes';
+import { healthRoutes } from '@health/routes/health.routes';
 import { postRoutes } from '@post/routes/post.routes';
 import { serverAdapter } from '@service/queues/base.queue';
 import { userRoutes } from '@user/routes/user.routes';
@@ -9,6 +10,12 @@ const BASE_PATH = '/api/v1';
 export default (app: Application) => {
   const routes = () => {
     app.use('/queues', serverAdapter.getRouter());
+
+    app.use('', healthRoutes.health());
+    app.use('', healthRoutes.env());
+    app.use('', healthRoutes.instance());
+    app.use('', healthRoutes.fiboRoutes());
+
     app.use(BASE_PATH, authRoutes.routes());
     app.use(BASE_PATH, userRoutes.routes());
     app.use(BASE_PATH, postRoutes.routes());

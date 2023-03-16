72d043fd4de6d49044253d0f83dd82251dcd84b3
Tai Nguyen <nphattai98@gmail.com>
2023-03-16 13:35:08 +0700

    chore: change terraform elasticache

--- deployment/7-elasticache.tf
+++ deployment/7-elasticache.tf
@@ -1,18 +1,33 @@
-
-resource "aws_elasticache_cluster" "example" {
-  cluster_id           = "funnychatapp-elasticache-cluster"
-  engine               = "redis"
-  engine_version       = "6.x"
-  node_type            = "cache.t2.micro"
-  num_cache_nodes      = 1
-  parameter_group_name = "default.redis6.x"
-  port                 = 6379
-  subnet_group_name    = aws_elasticache_subnet_group.elasticache_subnet_group.name
-  security_group_ids   = [aws_security_group.elasticache_sg.id]
-}
-
 resource "aws_elasticache_subnet_group" "elasticache_subnet_group" {
   name       = "${local.prefix}-subnet-elasticache-group"
   subnet_ids = [aws_subnet.private_subnet_a.id, aws_subnet.private_subnet_b.id]
 }
 
+resource "aws_elasticache_replication_group" "chatapp_redis_cluster" {
+  automatic_failover_enabled = true
+  replication_group_id       = "${local.prefix}-redis"
+  node_type                  = var.elasticache_node_type
+  description                = "Redis elasticache replication group"
+  num_cache_clusters         = 2
+  port                       = 6379
+  multi_az_enabled           = true
+  subnet_group_name          = aws_elasticache_subnet_group.elasticache_subnet_group.name
+  security_group_ids         = [aws_security_group.elasticache_sg.id]
+
+  depends_on = [
+    aws_security_group.elasticache_sg
+  ]
+
+  provisioner "local-exec" {
+    command = file("./userdata/update-env-file.sh")
+
+    environment = {
+      ELASTICACHE_ENDPOINT = self.primary_endpoint_address
+    }
+  }
+
+  tags = merge(
+    local.common_tags,
+    tomap({ "Name" = "${local.prefix}-elasticache" })
+  )
+}
